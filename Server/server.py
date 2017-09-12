acc7694b1c63c326d3dfb75e77a00178bd952633
Lewis Bell <lewisbell999@gmail.com>
2017-09-12 15:00:20 +0200

    Configure API Endpoints

--- Server/server.py
+++ Server/server.py
@@ -1,14 +1,30 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import json
 app = Flask(__name__)
+app.config['APPLICATION_ROOT'] = '/api/v1'
 
 
-@app.route('/')
+@app.route('/api/v1/')
 def foo():
     obj = {
         'version': '0.1',
     }
     return send_json(obj)
+
+
+@app.route('/api/v1/register', methods=['POST'])
+def register():
+    return send_json(request.form)
+
+
+@app.route('/api/v1/fetch/<ref_id>')
+def fetch(ref_id):
+    return 'fetch %s' % ref_id
+
+
+@app.route('/api/v1/inc/<ref_id>')
+def inc(ref_id):
+    return 'inc %s' % ref_id
 
 
 def send_json(data):
