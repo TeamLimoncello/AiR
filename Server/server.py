5a980743f6daf7f05de1dc410aac826d4e5f4f48
Lewis Bell <lewisbell999@gmail.com>
2017-09-12 10:44:23 +0200

    add jsonResponse

--- Server/server.py
+++ Server/server.py
@@ -1,3 +1,17 @@
+from flask import Flask, Response
+import json
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    pass
+
+@app.route('/')
+def foo():
+    obj = {
+        'version': '0.1',
+    }
+    return send_json(obj)
+
+
+def send_json(data):
+    return Response(response=json.dumps(data),
+                    status=200,
+                    mimetype="application/json")
