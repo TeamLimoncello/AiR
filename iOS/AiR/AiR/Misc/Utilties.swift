02ef737a612c49372b3571eff292c58af8faf23d
Jay Lees <jay.lees@me.com>
2017-09-12 10:53:09 +0200

    Started work on full AR view

---
+++ iOS/AiR/AiR/Misc/Utilties.swift
@@ -0,0 +1,14 @@
+//
+//  Utilties.swift
+//  AiR
+//
+//  Created by Jay Lees on 12/09/2017.
+//  Copyright © 2017 lemoncello. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+func degreesToRadians(_ degrees: Float) -> Float {
+    return degrees * Float.pi/180
+}
--- iOS/AiR/AiR/ViewController.swift
+++ iOS/AiR/AiR/ViewController.swift
@@ -7,18 +7,69 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    var mapPlaneNode: SCNNode!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sceneView.showsStatistics = true
+        let scene = SCNScene()
+        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.debugOptions = [.showConstraints, .showLightExtents, ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = AROrientationTrackingConfiguration()
+        sceneView.session.run(configuration)
+        addOuterSphere()
+        addPlane()
+    }
+    
+    fileprivate func addOuterSphere() {
+        // Create a double sided sphere of radius 1m
+        let outerSphere = SCNSphere(radius: 2.5)
+        outerSphere.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "testGradient")
+        outerSphere.firstMaterial?.isDoubleSided = true
+        
+        // Set the outer sphere node to the phones position (0, 0, 0)
+        let outerSphereNode = SCNNode(geometry: outerSphere)
+        outerSphereNode.position = SCNVector3(0, 0, 0)
+        
+        // Add the node to our world
+        sceneView.scene.rootNode.addChildNode(outerSphereNode)
+    }
+    
+    fileprivate func addPlane(){
+        let plane = SCNPlane(width: 100, height: 5)
+        plane.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "testMap")
+        plane.firstMaterial?.isDoubleSided = true
+        mapPlaneNode = SCNNode(geometry: plane)
+        mapPlaneNode.position = SCNVector3(-50, -1.4, 0)
+        mapPlaneNode.rotation = SCNVector4(1, 0, 0, degreesToRadians(90))
+        
+        sceneView.scene.rootNode.addChildNode(mapPlaneNode)
+        
+        animateMapMoving()
+    }
+    
+    func animateMapMoving(){
+        UIView.animate(withDuration: 10) {
+            
+            //NB: SCNVector IS NOT ANIMATABLE!!!!!!!!!!!!!!!!
+            //TODO: Make the thing move
+            self.mapPlaneNode.position = SCNVector3(x: 50, y: -1.4, z: 0)
+        }
+    }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
