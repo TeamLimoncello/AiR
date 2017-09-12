a423da6a2ef9b0312e2c58230c4079a66beb42d4
Lewis Bell <lewisbell999@gmail.com>
2017-09-12 16:25:05 +0200

    Process POST data from /register API Endpoint

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,6 @@
-from flask import Flask, Response, request
+import datetime
+import re
+from flask import Flask, Response, request, logging
 import json
 app = Flask(__name__)
 app.config['APPLICATION_ROOT'] = '/api/v1'
@@ -14,7 +16,19 @@ def foo():
 
 @app.route('/api/v1/register', methods=['POST'])
 def register():
-    return send_json(request.form)
+    try:
+        raw_date = request.form['date']
+        date = datetime.datetime.strptime(raw_date,'%Y-%M-%d')
+    except (KeyError, ValueError):
+        return send_json({'code': 1, 'string': 'Bad Date'}, 400)
+    try:
+        raw_flight = request.form['flightNumber']
+        flight_num = re.match(r'([0-9A-Z]{2})([A-Z]?)([0-9]{1,4})([A-Za-z]?)',raw_flight)
+        if flight_num is None:
+            raise ValueError('Invalid flight number')
+    except (KeyError, ValueError):
+        return send_json({'code': 2, 'string': 'Bad flight number'}, 400)
+    return send_json({'flight': flight_num.groups(), 'date': str(date)})
 
 
 @app.route('/api/v1/fetch/<ref_id>')
@@ -22,12 +36,18 @@ def fetch(ref_id):
     return 'fetch %s' % ref_id
 
 
-@app.route('/api/v1/inc/<ref_id>')
-def inc(ref_id):
-    return 'inc %s' % ref_id
+@app.route('/api/v1/reload/<ref_id>')
+def reload(ref_id):
+    return 'reload %s' % ref_id
 
 
-def send_json(data):
+@app.route('/api/v1/refetch/<ref_id>')
+def refetch(ref_id):
+    return
+
+
+
+def send_json(data, code=200):
     return Response(response=json.dumps(data),
-                    status=200,
+                    status=code,
                     mimetype="application/json")
