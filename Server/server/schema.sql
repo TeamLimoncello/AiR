8f5d11e9a6448dba6387169b85e88a2e9ad90779
Charlie Harding <xsanda@me.com>
2017-09-14 17:06:08 +0200

    Add flight path cache

--- Server/server/schema.sql
+++ Server/server/schema.sql
@@ -4,5 +4,13 @@ CREATE TABLE flightIDs (
   id Char(16) PRIMARY KEY,
   flightCode Char(8),
   date Char(10),
-  dataReady BOOLEAN
+  dataReady Boolean
+);
+
+DROP TABLE IF EXISTS flightPaths;
+
+CREATE TABLE flightPaths (
+  flightCode Char(8) PRIMARY KEY,
+  expires Integer,
+  path Text
 );
