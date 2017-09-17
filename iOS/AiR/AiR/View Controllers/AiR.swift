e37fb994f57848c8fb5e7a3d124fb48f3313da42
bluey31 <brendon.warwick@gmail.com>
2017-09-17 22:31:20 +0200

    Add landmark views

--- iOS/AiR/AiR/View Controllers/AiR.swift
+++ iOS/AiR/AiR/View Controllers/AiR.swift
@@ -18,23 +18,31 @@ class AiR: UIViewController {
     @IBOutlet weak var toggleSphereButton: UIButton!
     @IBOutlet weak var nightModeView: UIView!
     @IBOutlet weak var backView: UIView!
-    
+
+    // Significant Place View
     @IBOutlet var significantPlaceInfoView: UIView!
     @IBOutlet weak var significantPlaceName: UILabel!
     @IBOutlet weak var significantPlaceEnglishName: UILabel!
     @IBOutlet weak var significantPlacePopulation: UILabel!
-    
+
+    // Landmark Info View
+    @IBOutlet var landmarkInfoView: UIView!
+    @IBOutlet weak var landmarkName: UILabel!
+    @IBOutlet weak var landmarkHeight: UILabel!
+    @IBOutlet weak var landmarkEstablished: UILabel!
+    @IBOutlet weak var landmarkDescription: UILabel!
+
     var mapPlaneNode: SCNNode!
     var locationManager: CLLocationManager!
     var deviceHeading: CLLocationDirection?
     var mapGrid: MapGrid?
     var outerSphereNode: SCNNode!
     var flightPath: Path!
-    
+
     override var prefersStatusBarHidden: Bool {
         return true
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         toggleSphereView.layer.cornerRadius = 8
@@ -45,7 +53,7 @@ class AiR: UIViewController {
         //sceneView.debugOptions = [.showConstraints, .showLightExtents, ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         sceneView.isJitteringEnabled = true
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if AROrientationTrackingConfiguration.isSupported {
@@ -59,13 +67,13 @@ class AiR: UIViewController {
             createDialogue(title: "Error", message: "Your device does not support Augmented Reality apps.", parentViewController: self, dismissOnCompletion: true)
         }
     }
-    
+
     func styleButtons() {
         backView.addShadow(intensity: .Ehh)
         toggleSphereView.addShadow(intensity: .Ehh)
         nightModeView.addShadow(intensity: .Ehh)
     }
-    
+
     fileprivate func setupLocation(){
         locationManager = CLLocationManager()
         locationManager.requestWhenInUseAuthorization()
@@ -73,42 +81,50 @@ class AiR: UIViewController {
         locationManager.startUpdatingHeading()
         locationManager.startUpdatingLocation()
     }
-    
+
     fileprivate func addOuterSphere() {
         // Create a double sided sphere of radius 1m
         let outerSphere = SCNSphere(radius: 500)
         outerSphere.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "skyGradient")
         outerSphere.firstMaterial?.isDoubleSided = true
-        
+
         // Set the outer sphere node to the phones position (0, 0, 0)
         outerSphereNode = SCNNode(geometry: outerSphere)
         outerSphereNode.position = SCNVector3(0, 0, 0)
         outerSphereNode.isHidden = true
-        
+
         // Add the node to our world
         sceneView.scene.rootNode.addChildNode(outerSphereNode)
     }
-    
+
     fileprivate func addPlane(){
         mapGrid = MapGrid(deviceHeading: Float(deviceHeading!.magnitude), path: flightPath)
         sceneView.scene.rootNode.addChildNode(mapGrid!.mainPlaneNode)
-        
+
         self.mapGrid?.startFlight()
     }
-    
+
     fileprivate func setupInfoView(){
         significantPlaceInfoView.layer.cornerRadius = 16
         significantPlaceInfoView.center = self.sceneView.center
         significantPlaceInfoView.addShadow(intensity: .Ehh)
     }
-    
+
     func displayCity(_ place: City){
         significantPlaceName.text = place.name
         significantPlaceEnglishName.text = "English Name: \(place.englishName ?? place.name)"
         significantPlacePopulation.text =  "Population: \(place.population)"
         view.addSubview(significantPlaceInfoView)
     }
-    
+
+    func displayLandmark(_ landmark: Landmark){
+        landmarkName.text = landmark.name
+        landmarkHeight.text = "Height: \(landmark.height)"
+        landmarkEstablished.text =  "Established: \(landmark.established)"
+        landmarkDescription.text =  "\(landmark.description)"
+        view.addSubview(landmarkInfoView)
+    }
+
     //MARK: - Touch Methods
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
@@ -121,12 +137,12 @@ class AiR: UIViewController {
             }
         }
     }
-    
+
     //NodeIDs are given in the form {type}-{id}
     func showInformation(nodeID: String){
         let nodeID = nodeID.split(separator: "-")
         guard nodeID.count == 2 else { return }
-        
+
         switch nodeID[0] {
         case "city":
             let cityID = Int(String(Array(nodeID[1])))
@@ -134,30 +150,35 @@ class AiR: UIViewController {
             break
         case "landmark":
             print(nodeID[1])
+            let landmarkName = String(Array(nodeID[1]))
+            displayLandmark((flightPath.landmarks.filter({$0.name == landmarkName}).first)!)
             break
         default:
             print("Error whilst parsing touch")
         }
         
-        
     }
-    
+
     //MARK: - Action Methods
     @IBAction func exitARPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func toggleNightModePressed(_ sender: Any) {
          mapGrid?.changeTileType()
     }
-    
+
     @IBAction func toggleSpherePressed(_ sender: Any) {
         outerSphereNode.isHidden = !outerSphereNode.isHidden
         //toggleSphereButton.setBackgroundImage(outerSphereNode.isHidden ? #imageLiteral(resourceName: "Earth") : , for: .normal)
     }
-    
+
     @IBAction func significantPlaceBackPressed(_ sender: Any) {
         significantPlaceInfoView.removeFromSuperview()
+    }
+
+    @IBAction func landmarkBackPressed(_ sender: Any) {
+        landmarkInfoView.removeFromSuperview()
     }
 }
 
@@ -167,7 +188,7 @@ extension AiR: CLLocationManagerDelegate {
         manager.stopUpdatingHeading()
         addPlane()
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 //        if let grid = mapGrid {
 //            //grid.updateLocation(locations[0])
